16cd2a2edbb2737ad1b54101ae76dc9135c2aaa6
Jesse Eikema <jesse@voorhoede.nl>
2016-07-15 14:12:17 +0200

    feat(gifsicle): add logic for multiple files or added a thadeetje

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+'use strict';
 const execFile = require('child_process').execFile;
 const gifsicle = require('gifsicle');
+const glob = require('glob');
+const path = require('path');
+
+const files = glob.sync('./src/*.gif');
+
+console.log(gifsicle);
+//resize gif with a certain width of 200px and it automaticly adds the proportional height and optimization highest level
+//execFile(gifsicle, ['-o', 'temp/voorhoede_animatie-s.gif', 'src/voorhoede_animatie.gif','--resize-width', '200', '-O3'], function (err) {
+//    console.log('Image minified!', err);
+//});
 
 
-//resize gif with a certain width of 200px and it automaticly adds the proportional height
-execFile(gifsicle, ['-o', 'temp/voorhoede_animatie-s.gif', 'src/voorhoede_animatie.gif','--resize-width', '200'], function (err) {
-    console.log('Image minified!', err);
-});
+function makeGifPromise(file) {
+    let input = `src/${file}`;
+    let output = `temp/${file}`;
+    return new Promise(function(resolve, reject){
+        execFile(gifsicle, ['-o', output, input,'--resize-width', '200', '-O3'], (err) => {
+            if(err){
+                reject(err);
+            }
+            resolve();
+        });
+    })
+}
 
+Promise.all(files.map(makeGifPromise)).then(function(){console.log('hoppa')});
