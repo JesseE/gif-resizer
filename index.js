13873b65a1e758608e5273026f278b1347e2fb07
Jesse Eikema <jesse@voorhoede.nl>
2016-07-15 13:23:45 +0200

    resizer(gifsicle): added basic gifsicle prototype with resize a gif

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const execFile = require('child_process').execFile;
 const gifsicle = require('gifsicle');
 
+
+//resize gif with a certain width of 200px and it automaticly adds the proportional height
 execFile(gifsicle, ['-o', 'temp/voorhoede_animatie-s.gif', 'src/voorhoede_animatie.gif','--resize-width', '200'], function (err) {
     console.log('Image minified!', err);
 });
+
