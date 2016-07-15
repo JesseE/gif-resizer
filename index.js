cfc40ab5469b540a038582c145713a8e758a15e7
Jesse Eikema <jesse@voorhoede.nl>
2016-07-15 13:23:04 +0200

    resizer(gifsicle): added basic gifsicle prototype with resize a gif

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-const gulp = require('gulp');
-const debug = require('gulp-debug');
 const execFile = require('child_process').execFile;
-const concat = require('concat-frames');
-const resize = require('resizer-stream');
-const fs = require('fs');
-const neuquant = require('neuquant');
-const GIFEncoder = require('gif-stream/encoder');
-const rename = require('gulp-rename');
-const GIFDecoder = require('gif-stream/decoder');
+const gifsicle = require('gifsicle');
 
-
-fs.createReadStream('src/voorhoede_animatie.gif')
-    .pipe(new GIFDecoder())
-    .pipe(concat(function(frames) {
-        for (var i = 0; i < frames.length; i++) {
-            frames[i].width = 300;
-            frames[i].height = 200;
-            console.log(frames[i].width,frames[i].height);
-        }
-    }))
-    .pipe(fs.createWriteStream('temp/voorhoede_animatie.gif'));
+execFile(gifsicle, ['-o', 'temp/voorhoede_animatie-s.gif', 'src/voorhoede_animatie.gif','--resize-width', '200'], function (err) {
+    console.log('Image minified!', err);
+});
