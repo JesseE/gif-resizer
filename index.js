dd4ce4d0dc448b5f7c204ba61343e2ef68f55d77
Jesse Eikema <jesse@voorhoede.nl>
2016-07-18 11:02:14 +0200

    feat(index): added sizes  and correct way to have individual naming still need to refactor code though

--- index.js
+++ index.js
@@ -2,22 +2,54 @@
 const execFile = require('child_process').execFile;
 const gifsicle = require('gifsicle');
 const glob = require('glob');
-const path = require('path');
-
 const files = glob.sync('./src/*.gif');
+const fs = require('fs');
 
-console.log(gifsicle);
-//resize gif with a certain width of 200px and it automaticly adds the proportional height and optimization highest level
-//execFile(gifsicle, ['-o', 'temp/voorhoede_animatie-s.gif', 'src/voorhoede_animatie.gif','--resize-width', '200', '-O3'], function (err) {
-//    console.log('Image minified!', err);
-//});
-
-
+//resize gif with a certain width of 200px and it automatically adds the proportional height and optimization highest level
 function makeGifPromise(file) {
+    file = file.split("./src/").pop();
+    let size = {
+        's': {
+            'tag' : '-s',
+            'width': 200
+        },
+        'm': {
+            'tag' : '-m',
+            'width': 600
+        },
+        'l': {
+            'tag' : '-l',
+            'width': 900
+        }
+    };
+
+    //need to find something smarter than this solution
+    let fileBasename = file.slice(0, -4);
+    let extension = '.gif';
+
+    //fileBasename = fileBasename += size.s.tag += extension;
     let input = `src/${file}`;
-    let output = `temp/${file}`;
+
+    //this should be refactored or solved smarter
+    let output_s = `temp/${fileBasename += size.s.tag += extension}`;
+    let output_m = output_s.replace('-s', '-m');
+    let output_l = output_m.replace('-m', '-l');
+
+    //dont repeat you self stuff
     return new Promise(function(resolve, reject){
-        execFile(gifsicle, ['-o', output, input,'--resize-width', '200', '-O3'], (err) => {
+        execFile(gifsicle, ['-o', output_s, input,'--resize-width', size.s.width, '-O3'], (err) => {
+            if(err){
+                reject(err);
+            }
+            resolve();
+        });
+        execFile(gifsicle, ['-o', output_m, input,'--resize-width', size.m.width, '-O3'], (err) => {
+            if(err){
+                reject(err);
+            }
+            resolve();
+        });
+        execFile(gifsicle, ['-o', output_l, input,'--resize-width', size.l.width, '-O3'], (err) => {
             if(err){
                 reject(err);
             }
@@ -26,4 +58,4 @@ function makeGifPromise(file) {
     })
 }
 
-Promise.all(files.map(makeGifPromise)).then(function(){console.log('hoppa')});
+Promise.all(files.map(makeGifPromise)).then(function(){console.log('gifs resized')});
