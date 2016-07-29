776992849fded5f9d9f2c0b63a54487fdb2e4eed
Jesse Eikema <jesse@voorhoede.nl>
2016-07-29 11:20:50 +0200

    gif resizer refactored

--- index.js
+++ index.js
@@ -2,60 +2,36 @@
 const execFile = require('child_process').execFile;
 const gifsicle = require('gifsicle');
 const glob = require('glob');
-const files = glob.sync('./src/*.gif');
+const path = require('path');
+const files = glob.sync('./src/*.gif', {root: './src/'});
 const fs = require('fs');
 
-//resize gif with a certain width of 200px and it automatically adds the proportional height and optimization highest level
-function makeGifPromise(file) {
-    file = file.split("./src/").pop();
-    let size = {
-        's': {
-            'tag' : '-s',
-            'width': 200
-        },
-        'm': {
-            'tag' : '-m',
-            'width': 600
-        },
-        'l': {
-            'tag' : '-l',
-            'width': 900
+function resizeGIF(file) {
+
+    const sizes = [
+        { suffix: '-s', width: 200 },
+        { suffix: '-m', width: 600 },
+        { suffix: '-l', width: 1440 }
+    ];
+
+    return Promise.all(sizes.map(size => {
+
+        return new Promise((resolve, reject) => {
+
+            let fileBasename = path.basename(file);
+            let fileExtension = path.extname(file);
+            let fileBasenameNoExtension = fileBasename.slice(0,-1 * fileExtension.length);
+
+            const input = `src/${fileBasename}`;
+            const output = `temp/${fileBasenameNoExtension}${size.suffix}${fileExtension}`;
+
+            //gifsicle used for resizing the input. -o is the oporation send output to file  and -03 is the maximum gif optimization level
+            execFile(gifsicle, ['-o', output, input,'--resize-width', size.width, '-O3'], err => {
+                err ? reject(err) : resolve(output);
+            });
         }
-    };
-
-    //need to find something smarter than this solution
-    let fileBasename = file.slice(0, -4);
-    let extension = '.gif';
-
-    //fileBasename = fileBasename += size.s.tag += extension;
-    let input = `src/${file}`;
-
-    //this should be refactored or solved smarter
-    let output_s = `temp/${fileBasename += size.s.tag += extension}`;
-    let output_m = output_s.replace('-s', '-m');
-    let output_l = output_m.replace('-m', '-l');
-
-    //dont repeat you self stuff
-    return new Promise(function(resolve, reject){
-        execFile(gifsicle, ['-o', output_s, input,'--resize-width', size.s.width, '-O3'], (err) => {
-            if(err){
-                reject(err);
-            }
-            resolve();
-        });
-        execFile(gifsicle, ['-o', output_m, input,'--resize-width', size.m.width, '-O3'], (err) => {
-            if(err){
-                reject(err);
-            }
-            resolve();
-        });
-        execFile(gifsicle, ['-o', output_l, input,'--resize-width', size.l.width, '-O3'], (err) => {
-            if(err){
-                reject(err);
-            }
-            resolve();
-        });
-    })
+      )
+    }));
 }
 
-Promise.all(files.map(makeGifPromise)).then(function(){console.log('gifs resized')});
+Promise.all(files.map(resizeGIF)).then(function(){console.log('gifs resized')});
